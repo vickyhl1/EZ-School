7249496ffa72a6b0726f99ccb0b955fc4765f8c1
vicky <vickyhl@ac.sce.ac.il>
2020-11-29 13:31:48 +0200

    Users update

--- hackaton/data/Users.py
+++ hackaton/data/Users.py
@@ -4,7 +4,7 @@ class Users:
     Id = mongoengine.StringField(required=True)
     Passwords = mongoengine.StringField(required=True)
     Usertype = mongoengine.IntField(required=True, min_value=1, max_value=3)
-    
+
 
 meta = {
     'db_alias': 'core',
