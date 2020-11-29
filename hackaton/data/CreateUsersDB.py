ba229379ea46eed39e6b38c85ddc0e06fcbe26dc
Rafael Azriaiev <rafaeaz@ac.sce.ac.il>
2020-11-29 18:50:56 +0200

    user db creation

--- hackaton/data/CreateUsersDB.py
+++ hackaton/data/CreateUsersDB.py
@@ -1,29 +1,47 @@
 import pymongo
-
+import pprint
 client = pymongo.MongoClient()
 mydb = client['EZSchooldb']
 
 mycol1 = mydb['Inventory']
 mycol2 = mydb['Users']
 #1- Secreatery, 2- Teacher, 3-Student
+
 data = {
     'name': 'Rafael Azriaiev',
     'id':   '309044071',
     'password': '1234',
     'Usertype': 3
 }
-mycol2.insert_one(data)
+user = mycol2.find_one({'id': data['id']})
+if user == None:
+    mycol2.insert_one(data)
+elif not user['id'] == data['id']:
+    mycol2.insert_one(data)
+
+
 data = {
     'name': 'Tal Shaked',
     'id':   '312208523',
     'password': '12345',
     'Usertype': 2
 }
-mycol2.insert_one(data)
+user = mycol2.find_one({'id': data['id']})
+if user == None:
+    mycol2.insert_one(data)
+elif not user['id'] == data['id']:
+    mycol2.insert_one(data)
+
+
 data = {
     'name': 'Yovel Aloni',
     'id':   '319122842',
     'password': '123456',
     'Usertype': 1
 }
-mycol2.insert_one(data)
+user = mycol2.find_one({'id': data['id']})
+if user == None:
+    mycol2.insert_one(data)
+elif not user['id'] == data['id']:
+    mycol2.insert_one(data)
+
