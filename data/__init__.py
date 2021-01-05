9aa9adccade94b012910c8ae93c2acd0efd98460
vicky <vickyhl@ac.sce.ac.il>
2021-01-05 17:02:34 +0200

    Main menu design

--- data/__init__.py
+++ data/__init__.py
@@ -2,14 +2,9 @@
 
 from .CreateUsersDB import user_db_init
 from .CreateInventoryDB import inventory_db_init
-<<<<<<< HEAD
-=======
 from .CreateGameLinkDB import game_links_db_init
 from .CreateSekerDB import seker_db_init
 from .CreateInventoryDB import inventory_db_init, connect_to_collection
-
-
->>>>>>> 26bbc8bc7e7fd9fa7f42a3ca58d28182257d8648
 from .CreateScheduleClassesDB import Schedule_db_init
 from .CreateGameLinkDB import game_links_db_init
 from .CreateSekerDB import seker_db_init
